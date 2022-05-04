dfba9208042d1d66b26cbdbfda2e61eecfec1f8c
Jason Antalis <jason.antalis@gmail.com>
2022-05-04 21:30:05 +0200

    Added square brackets to work in lexer

--- Main.java
+++ Main.java
@@ -7,11 +7,7 @@ public class Main {
            return;
         }
         lex.printTokens();
-
-        System.out.println("\nAnother test file:\n");
-        Lexer test = new Lexer("yingTest.txt");
-        test.run();
-        test.printTokens();
+        
     }
 
 }
